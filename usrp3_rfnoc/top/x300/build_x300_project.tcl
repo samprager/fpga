210e432d206d37542da275415b7643895b8436e7
samprager <sprager@usc.edu>
2016-10-27 19:32:55 -0700

    update top level gitignore

---
+++ usrp3_rfnoc/top/x300/build_x300_project.tcl
@@ -0,0 +1,9 @@
+#
+# Copyright 2014-2015 Ettus Research
+#
+
+source $::env(VIV_TOOLS_DIR)/scripts/viv_utils.tcl
+source $::env(VIV_TOOLS_DIR)/scripts/viv_strategies.tcl
+
+# STEP#1: Create project, add sources, refresh IP
+vivado_utils::initialize_project
